e7e9a0a32cbea17334cb09911493123c565b9666
Renan Frutuozo <frutuozo29@gmail.com>
2017-04-22 13:59:39 -0300

    Criando o footer do layout.cshtml

---
+++ back-end/Fantasy-Surf/src/Fantasy-Surf.Domain/Entities/RegrasFantasy.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Fantasy_Surf.Domain.Entities
+{
+    public class RegrasFantasy
+    {
+        public Guid RegrasFantasyId { get; set; }
+        public string Descricao { get; set; }
+    }
+}
---
+++ back-end/Fantasy-Surf/src/Fantasy-Surf.Presentation.MVC/ViewModel/RegrasFantasyViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Fantasy_Surf.Presentation.MVC.ViewModel
+{
+    public class RegrasFantasyViewModel
+    {
+        public long Id { get; set; }
+        public string Descricao { get; set; }
+    }
+}
